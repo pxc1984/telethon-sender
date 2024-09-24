9a7bfe18dc47bd14f6f4a2337e49dc31bf2a7562
pxc1984 <igor.mamaev1@gmail.com>
2024-09-25 01:58:45 +0300

    Добавил гораздо больше кастомизации через config.toml, добавил поддержку нескольких сообщений

--- main.py
+++ main.py
@@ -1,43 +1,12 @@
 #!/usr/bin/env python3
 
-import random
 import logging
 import asyncio
 import toml
 import datetime
+import random
 from telethon import TelegramClient
 from telethon.errors import RPCError, PeerFloodError, ChatAdminRequiredError, UserNotParticipantError
-
-
-config = toml.load('config.toml')
-
-if ('telegram' in config):
-    api_id = config['telegram']['api_id']
-    api_hash = config['telegram']['api_hash']
-    phone_number = config['telegram']['phone_number']
-    channel_id: list[str] = config['telegram']['channel_id'].split(';')
-else:
-    print("config.toml not configured properly")
-    exit(1)
-
-DEBUG = False
-if ('debug' in config):
-    DEBUG = config['debug']['enabled']
-
-client = TelegramClient('anon', api_id, api_hash)
-
-call_stack = []
-
-logger = logging.getLogger("main_logger")
-logger.setLevel(logging.DEBUG)
-
-file_handler = logging.FileHandler('sent.log')
-file_handler.setLevel(logging.INFO)
-
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-file_handler.setFormatter(formatter)
-
-logger.addHandler(file_handler)
 
 
 class GoodNightNotifier:
@@ -46,22 +15,12 @@ class GoodNightNotifier:
         
         self.prev_notify = datetime.datetime.now()
         self.next_notify = self.prev_notify
-        
-        if not DEBUG:
-            self.min_delta = 23 * 60 * 60  # 23 часа в секундах
-            self.max_delta = 25 * 60 * 60  # 25 часов в секундах
-        else:
-            self.min_delta = 1
-            self.max_delta = 10
-        
-        self.notify_text = [ "Спокойной ночи" ]
     
     def get_text(self) -> str:
-        return random.choice(self.notify_text)
+        return random.choice(messages)
     
     def set_time(self) -> None:
-        # Устанавливаем следующее уведомление с рандомной задержкой от 23 до 25 часов
-        delta_seconds = random.randint(self.min_delta, self.max_delta)
+        delta_seconds = base_step + random.randint(0, delta_minus + delta_plus) - delta_minus
         self.next_notify = self.prev_notify + datetime.timedelta(seconds=delta_seconds)
 
     async def run(self) -> None:
@@ -70,25 +29,49 @@ class GoodNightNotifier:
             self.prev_notify = datetime.datetime.now()
             self.set_time()
 
-    async def send_message(self) -> bool:
-        message = self.get_text()
+    async def send_message(self, message=None) -> None:
+        if message == None:
+            message = self.get_text()
+        
         await client.send_message(self.entity, message)
         logger.info(f"Sent message \"{message}\" to {self.entity.access_hash} with delay {datetime.datetime.now() - self.prev_notify}")
-        return True
+
+
+def load_messages() -> None:
+    global messages
+    with open("messages.dat", mode="r") as f:
+        messages = [i.strip() for i in f.readlines()]
+    return
+
+def setup_logging():
+    global logger, file_handler, formatter
+
+    logger = logging.getLogger("main_logger")
+    logger.setLevel(logging.DEBUG)
+
+    file_handler = logging.FileHandler(log_file_name)
+    file_handler.setLevel(logging.INFO)
+
+    formatter = logging.Formatter(log_format)
+    file_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
 
 async def setup_handlers():
+    global call_stack
+    call_stack = []
     for i in channel_id:
         try: 
             entity = await client.get_input_entity(i)
-            
             call_stack.append(GoodNightNotifier(entity))
+            
             logger.info(f"Call generated for {entity.access_hash} successfully")
         except RPCError as e:
-            print(f"Failed to send message: {e}")
+            logger.error(f"Failed to send message: {e}")
         except ValueError as e:
-            print(f"ValueError: {e}. Please check the channel ID.")
+            logger.error(f"ValueError: {e}. Please check the channel ID.")
         except (PeerFloodError, ChatAdminRequiredError, UserNotParticipantError) as e:
-            print(f"Error related to permissions or bot participation: {e}")
+            logger.error(f"Error related to permissions or bot participation: {e}")
 
 
 async def run_bot():
@@ -98,12 +81,63 @@ async def run_bot():
         for handler in call_stack:
             await handler.run()
         
-        if DEBUG:
-            await asyncio.sleep(1)
+        await asyncio.sleep(60)
+
+def load_config():
+    global config, api_id, api_hash, phone_number, channel_id, DEBUG, client_name, \
+        delay, enable_randomization, base_step, delta_minus, delta_plus, \
+            log_file_name, log_format
+    
+    config = toml.load('config.toml')
+    
+    if ('app' in config):
+        client_name = config['app']['client_name']
+        delay = config['app']['delay']
+    else:
+        print("config.toml not configured properly [app section]")
+        exit(1)
+    
+    if ('randomization' in config):
+        enable_randomization = config['randomization']['enabled']
+        if enable_randomization:
+            base_step = config['randomization']['base_step']
+            delta_minus = config['randomization']['delta_minus']
+            delta_plus = config['randomization']['delta_plus']
         else:
-            await asyncio.sleep(60)
+            if ('base_step' in config['randomization']):
+                base_step = config['randomization']['base_step']
+            else:
+                base_step = 86400
+            delta_minus = 0
+            delta_plus = 0
+    else:
+        base_step = 86400
+        delta_minus = 0
+        delta_plus = 0
+    
+    if ('logging' in config):
+        log_file_name = config['logging']['file']
+        log_format = config['logging']['format']
+    else:
+        print("config.toml not configured properly [logging section]")
+        exit(1)
+
+    if ('telegram' in config):
+        api_id = config['telegram']['api_id']
+        api_hash = config['telegram']['api_hash']
+        phone_number = config['telegram']['phone_number']
+        channel_id = config['telegram']['channel_id'].split(';')
+    else:
+        print("config.toml not configured properly [telegram section]")
+        exit(1)
 
 
 if __name__ == '__main__':
+    load_config()
+    setup_logging()
+    load_messages()
+    
+    client = TelegramClient(client_name, api_id, api_hash)
+    
     loop = asyncio.get_event_loop()
     loop.run_until_complete(run_bot())
